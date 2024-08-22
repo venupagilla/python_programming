f6e827599e640ae1d4cb29f1c7a7d1e39cc4e902
venupagilla <pagillavenu909@gmail.com>
2024-08-22 19:43:59 +0530

    created program for reading values from keyboard

---
+++ week 1/Read_from_Keyboard.py
@@ -0,0 +1,9 @@
+#Reading input from keyboard
+name=input("Enter your name : ")
+address=input("Enter your adress : ")
+email=input("enter your email id : ")
+phone_no=int(input("enter your phone number : "))
+
+#printing the inserted values
+print("Below are the entered details")
+print(f"name is {name}\naddress is {address}\nemail is {email}\nphone number is {phone_no}")
