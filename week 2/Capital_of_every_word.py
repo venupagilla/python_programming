d6813f3d81c70dd675aa051d1be7386013368a43
venupagilla <pagillavenu909@gmail.com>
2024-09-03 22:08:07 +0530

    deleted the wrong file

--- week 2/Capital_of_every_word.py
+++
@@ -1,8 +0,0 @@
-letter=input("Enter the sentence you want to capitalize : ")
-new_letter=""
-for i in range(len(letter)):
-    if letter[i-1] == " " or i==0:
-        new_letter+=letter[i].upper()
-    else:
-        new_letter+=letter[i]
-print(new_letter)
