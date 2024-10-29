d5c95a81894f9f7ac455a75972ea1cf8a89171df
venu pagilla <94222926+venupagilla@users.noreply.github.com>
2024-10-29 16:21:20 +0530

    Delete week 1/compound_interest.py

--- week 1/compound_interest.py
+++
@@ -1,9 +0,0 @@
-def calc(princ,interest,time):
-    rate=interest/100
-
-    return princ*(1+rate)**time
-
-principal=100000
-interest=12
-time=3
-print(f"compund interest is {calc(principal,interest,time)}")
