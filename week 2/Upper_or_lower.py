a575df74b33c2eaecd17c9a748fc70eb2650eb0c
venu pagilla <94222926+venupagilla@users.noreply.github.com>
2024-10-29 16:22:09 +0530

    Delete week 2/Upper_or_lower.py

--- week 2/Upper_or_lower.py
+++
@@ -1,7 +0,0 @@
-n=input("Enter a number or letter")
-if n.isdigit():
-    print("Entered character is a digit")
-elif n.islower():
-    print("Entered character is a lower letter")
-else:
-    print("Entered character is a Upper letter")
